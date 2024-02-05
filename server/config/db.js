ca65411637331378da5c5d349563d8aebd3cfe42
soka1998 <soukainaharifi18@gmail.com>
2024-02-05 21:35:17 +0000

    configure express connection with MongoDB

---
+++ server/config/db.js
@@ -0,0 +1,32 @@
+
+import express from 'express';
+import mongoose, { connect } from 'mongoose';
+import {config} from "dotenv";
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+config()
+
+
+const app =  express();
+app.use(cors())
+app.use(express.json());
+app.use(cookieParser())
+// app.use("/api",router)
+// app.use("/user",UserRouter)  
+
+const PORT = process.env.PORT || 4000
+
+connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log("mongodb connected successfully")
+})
+.catch ((error) => {
+    console.log("mongodb not connected ", error)
+})      
+
+// swaggerDocs(app, PORT )
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
