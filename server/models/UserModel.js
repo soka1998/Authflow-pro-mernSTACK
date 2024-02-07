095fe2cc62b9b92a79db92284ba3f4d1e9a856ac
soka1998 <soukainaharifi18@gmail.com>
2024-02-07 12:32:26 +0000

    Delete Permission

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -16,11 +16,8 @@ const userSchema = new mongoose.Schema({
      type: String,
       required:[true, 'please enter a valid email'],
       minlength: [6, 'At least enter 6 character'],
-    },
-  roles: { 
-    type: Array, 
-    default: ["user"] }, // Par défaut, chaque utilisateur a le rôle 'user'
-});
+    }
+  });
 
 const UserModel = mongoose.model("User", userSchema);
 
