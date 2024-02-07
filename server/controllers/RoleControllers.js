24bdc2f80efaf9cc70eff571a5433b316d047137
soka1998 <soukainaharifi18@gmail.com>
2024-02-07 15:33:14 +0000

    update role controller

--- server/controllers/RoleControllers.js
+++ server/controllers/RoleControllers.js
@@ -1,48 +1,24 @@
-import  UserModel from '../models/UserModel';
+import Role from "../models/RoleModel";
 
-// Defining the RoleController object that will contain methods for managing roles
-const RoleController = {
-  // Attribuer un rôle à un utilisateur
-  async assignRole(req, res) {
-    try {
-      const { userId, roleName } = req.body;
+//Controller methods dor handling role-related operations
 
-       // Vérifier si l'utilisateur existe
-       const user = await UserModel.findById(userId);
-       if (!user) {
-         return res.status(404).json({ message: 'Utilisateur non trouvé.' });
-       }
-       
-       
-      // Vérifier si le rôle existe déjà dans les rôles de l'utilisateur
-      if (user.roles.includes(roleName)) {
-        return res.status(400).json({ message: 'L\'utilisateur a déjà ce rôle.' });
-      }
- 
-       // Ajouter le nouveau rôle à l'utilisateur
-       user.roles.push(roleName);
-
-       // Sauvegarder les modifications dans la base de données
-       await user.save();
- 
-       return res.status(200).json({ message: 'Rôle attribué avec succès.' }); 
-
-    }catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: 'Erreur lors de l\'attribution du rôle.' });
-      }},
-    // Récupérer la liste des rôles
-  async getRoles(req, res) {
-    try {
-      // Vous pouvez ajuster cette requête pour obtenir uniquement les rôles distincts
-      const roles = await UserModel.distinct('roles');
-
-      return res.status(200).json({ roles });
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ message: 'Erreur lors de la récupération des rôles.' });
+const RoleController = async (req, res) => {
+  try {
+    const { name } = req.body;
+    //check if the role with the same name already exists
+    const existingRole = await Role.findOne({ name });
+    if (existingRole) {
+      return res
+        .status(400)
+        .json({ error: "Role with this name alreadyexist " });
     }
-  },
-}
+    //Create new Role
+    const newRole = await Role.create({ name });
+    res.status(201).json(newRole);
+  } catch (error) {
+    console.error("Error creating role :", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
 
-  module.exports = RoleController;
+module.exports = RoleController;
