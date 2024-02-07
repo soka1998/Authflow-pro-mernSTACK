4dff1abb444901f5faf330401e531946236d005e
soka1998 <soukainaharifi18@gmail.com>
2024-02-07 12:10:38 +0000

    Retrieve the permission list

--- server/controllers/PermissionController.js
+++ server/controllers/PermissionController.js
@@ -1,31 +1,46 @@
-import Permission from '../models/PermissionModel'
+import Permission from "../models/PermissionModel";
 //create a new permission
-const PermissionController = { async createPermission(req,res){
-    try { 
-        const {name} = req.body;
+const PermissionController = {
+  async createPermission(req, res) {
+    try {
+      const { name } = req.body;
 
-        //Verify if the permission is already exist 
-        const existingPermission = await PermissionModel.findOne({ name });
-        
-        if (existingPermission){
-            return res.status(409).json({message: "This permission already exists"});
-        }
+      //Verify if the permission is already exist
+      const existingPermission = await PermissionModel.findOne({ name });
 
-      //Create a new permission 
-      const newPermission = new PermissionModel({name})
+      if (existingPermission) {
+        return res
+          .status(409)
+          .json({ message: "This permission already exists" });
+      }
 
-      //save the permission in database 
+      //Create a new permission
+      const newPermission = new PermissionModel({ name });
+
+      //save the permission in database
       await newPermission.save();
-      return res.status(201).json({message:'Pemission created successfully'})
-        
+      return res
+        .status(201)
+        .json({ message: "Pemission created successfully" });
     } catch (error) {
-        console.log(error);
-        return res.status(201).json({message:'Error accured when creating the permission'})
-
-        
+      console.log(error);
+      return res
+        .status(201)
+        .json({ message: "Error accured when creating the permission" });
     }
-
-}
-}
+  },
+  //Retrive permission list
+  async getPermission(req, res) {
+    try {
+      const permissions = await PermissionModel.find();
+      return res.status(200).json({ permissions });
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(500)
+        .json({ message: "Eroor while retrieving permissions list" });
+    }
+  },
+};
 
 module.exports = PermissionController;
