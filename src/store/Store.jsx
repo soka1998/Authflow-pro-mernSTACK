f49353b173e78b70d85f49c330f39e41337734ec
soka1998 <soukainaharifi18@gmail.com>
2024-02-10 21:44:32 +0000

    create the store

---
+++ src/store/Store.jsx
@@ -0,0 +1,9 @@
+// store.js
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './auth/authSlice';
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+});
