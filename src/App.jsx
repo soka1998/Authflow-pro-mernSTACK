70a82c8adcd0064230b53e3928b4f15fcaeff243
soka1998 <soukainaharifi18@gmail.com>
2024-02-11 23:26:45 +0000

    update components

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,13 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-// import { Route , Routes } from 'react-router-dom'
-import SignUp from './pages/SignUp.jsx'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 
 
 function App() {
   
 
   return (
-    <Router>
-     <Routes>
-      <Route path='/' element={<SignUp />}/>
-     </Routes>
-     </Router>
-    
+     <h1>hello</h1>
   )
 }
 
--- src/store/Store.jsx
+++
@@ -1,9 +0,0 @@
-// store.js
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './auth/authSlice';
-
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-  },
-});
