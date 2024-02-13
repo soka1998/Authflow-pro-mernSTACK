de5868dafb50b7173e1594bc2045a1d5df93b3fd
soka1998 <soukainaharifi18@gmail.com>
2024-02-13 12:19:55 +0000

    create login redux

---
+++ src/components/LoginRedux.jsx
@@ -0,0 +1,29 @@
+// components/Login.js
+
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { login } from '../redux/AuthSlice';
+
+function LoginRedux() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(login({ email, password }));
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginRedux;
